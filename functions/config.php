04904a493a78bba7ecb1cc5bd62a89f19cb9461b
augustocesar12 <augusto-felicio@hotmail.com>
2023-10-13 11:26:42 -0300

    Teste Mysql Conn

--- functions/config.php
+++ functions/config.php
@@ -4,3 +4,11 @@ const USER = 'sitedoau_admin_hortec';
 const DB   = 'sitodoau_db_hortec';
 const PASS = 'JKE!I?B-A-WL';
 const PORT = '3306';
+
+$conn = mysqli_connect(HOST, USER, PASS, DB, );
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
